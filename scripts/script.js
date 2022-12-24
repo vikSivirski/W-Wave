8291403e714520e70b7d90a5ba1157fd1caa5639
Viktor Brainiac <msne1488@gmail.com>
2022-12-24 20:51:18 +0300

    start broadcast

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const moreBtn = document.querySelector('.podcasts__more');
+const podcasts = document.querySelectorAll('.podcasts__item');
+
+moreBtn.addEventListener('click', function () {
+
+  moreBtn.classList.toggle('podcasts__more--hidden')
+  podcasts.forEach(function (element) {
+    element.classList.toggle('podcasts__item--visible')
+  });
+
+
+})
+
+// const like = document.querySelectorAll('.podcasts__reaction-btn');
+
+// like.forEach(likeEl => {
+//   const likeCounter = document.querySelector('.podcasts__readtion-counter');
+//   let counter = 0;
+
+//   likeEl.addEventListener('click', function () {
+//     render(++counter, likeCounter);
+//   });
+// });
+
+// const render = (counter, likeCounter) => likeCounter.innerText = counter;
+
