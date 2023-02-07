c1ba6dfe83fec9a8fc1c49bd7f207fa1bf0c2ce5
Viktor Brainiac <msne1488@gmail.com>
2023-02-07 19:45:12 +0300

    likes sistem

--- scripts/script.js
+++ scripts/script.js
@@ -6,7 +6,59 @@ burger.addEventListener('click', function() {
   burger.classList.toggle('is-active');
   menu.classList.toggle('header__nav--active');
 
-})
+});
+
+const searchBtn = document.querySelector('.header__search-btn ');
+const searchForm = document.querySelector('.search-form');
+const searchBtnActive = document.querySelector('.search-form__btn');
+searchBtn.addEventListener('click', function() {
+
+  searchForm.classList.toggle('search-form--active')
+
+});
+searchBtnActive.addEventListener('click', function() {
+
+  searchForm.classList.remove('search-form--active')
+
+});
+
+let likeBtn = document.querySelectorAll('.like');
+let shareBtn = document.querySelectorAll('.share');
+let listeningsBtn = document.querySelectorAll('.podcasts__play-btn')
+let likeCounter = document.querySelectorAll('.like__counter');
+let shareCounter = document.querySelectorAll('.share__counter');
+let listeningsCounter = document.querySelectorAll('.listenings__counter');
+
+function counterLikes() {
+  likeCounter.forEach(function(elem) {
+      let likes = parseInt(elem.textContent);
+      elem.textContent = likes + 1;
+  });
+};
+likeBtn.forEach(function(like) {
+  like.addEventListener('click', counterLikes)
+});
+
+function counterShare() {
+  shareCounter.forEach(function(elem) {
+      let likes = parseInt(elem.textContent);
+      elem.textContent = likes + 1;
+  });
+};
+shareBtn.forEach(function(share) {
+  share.addEventListener('click', counterShare)
+});
+
+function counterListenings() {
+  listeningsCounter.forEach(function(elem) {
+      let likes = parseInt(elem.textContent);
+      elem.textContent = likes + 1;
+  });
+};
+listeningsBtn.forEach(function(listenings) {
+  listenings.addEventListener('click', counterListenings)
+});
+
 
 
 const moreBtn = document.querySelector('.podcasts__more');
@@ -19,8 +71,7 @@ moreBtn.addEventListener('click', function () {
     element.classList.toggle('podcasts__item--visible')
   });
 
-
-})
+});
 
 
 const element = document.querySelector('.broadcast__select');
@@ -28,7 +79,6 @@ const choices = new Choices(element, {
 
   searchEnabled: false,
   itemSelectText: '',
-  // placeholder: true,
 
 });
 
